15998cc6d4cd66ef63940d2ecdf0f66fdc643529
Winston Pemberton <winstonpemberton@gmail.com>
2020-03-17 22:30:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    @num1 = params[:num1].to_f
+    @num1 = params[:num1]
     @num2 = params[:num2].to_f
     @num1 * @num2
   end
